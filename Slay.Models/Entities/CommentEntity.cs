1e1037219fa659f6754ad8a014bc7f99450c23ae
arunselvakumar <b.arunselvakumar@gmail.com>
2018-06-16 08:49:58 +0000

    Migrated to .NET Core 2.1

--- Slay.Models/Entities/CommentEntity.cs
+++ Slay.Models/Entities/CommentEntity.cs
@@ -1,39 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using Newtonsoft.Json;
-using Slay.Models.Entities.Interfaces;
-
-namespace Slay.Models.Entities
+﻿namespace Slay.Models.Entities
 {
-	public sealed class CommentEntity : IEntity
-	{
-		[BsonId]
-		[JsonProperty(PropertyName = "id")]
-		public ObjectId Id { get; set; }
+    using System;
 
-		[JsonProperty(PropertyName = "parentid")]
-		public string ParentId { get; set; }
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
 
-		[JsonProperty(PropertyName = "postid")]
-		public string PostId { get; set; }
+    using Newtonsoft.Json;
 
-		[JsonProperty(PropertyName = "comment")]
-		public string Comment { get; set; }
+    using Slay.Models.Entities.Interfaces;
 
-		[JsonProperty(PropertyName = "isdeleted")]
-		public bool IsDeleted { get; set; }
+    public sealed class CommentEntity : IEntity
+    {
+        [JsonProperty(PropertyName = "parentid")]
+        public string ParentId { get; set; }
 
-		[JsonProperty(PropertyName = "commentedby")]
-		public string CommentedBy { get; set; }
+        [JsonProperty(PropertyName = "postid")]
+        public string PostId { get; set; }
 
-		[JsonProperty(PropertyName = "createdon")]
-		public DateTime CreatedOn { get; set; }
+        [JsonProperty(PropertyName = "comment")]
+        public string Comment { get; set; }
 
-		public void Delete()
-		{
-			this.IsDeleted = true;
-		}
-	}
+        [JsonProperty(PropertyName = "commentedby")]
+        public string CommentedBy { get; set; }
+
+        [JsonProperty(PropertyName = "createdon")]
+        public DateTime CreatedOn { get; set; }
+
+        [BsonId]
+        [JsonProperty(PropertyName = "id")]
+        public ObjectId Id { get; set; }
+
+        [JsonProperty(PropertyName = "isdeleted")]
+        public bool IsDeleted { get; set; }
+
+        public void Delete()
+        {
+            this.IsDeleted = true;
+        }
+    }
 }
