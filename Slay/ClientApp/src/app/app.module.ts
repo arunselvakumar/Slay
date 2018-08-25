551449e3537e6d436908d71ebb0757887134567f
arunselvakumar <b.arunselvakumar@gmail.com>
2018-08-25 08:18:14 +0000

    #10 Integrated Angular Components for Posts

--- Slay/ClientApp/src/app/app.module.ts
+++ Slay/ClientApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationHeaderComponent } from './navigation-header/navigation-header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationHeaderComponent,
+    PostListComponent,
+    PostComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule
