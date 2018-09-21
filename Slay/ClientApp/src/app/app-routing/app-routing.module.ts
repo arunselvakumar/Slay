d1204bb357dfd1ec7cb233f5cb8b9f362f307e77
arunselvakumar <b.arunselvakumar@gmail.com>
2018-09-21 17:46:50 +0000

    Navigation Issue Fixed

--- Slay/ClientApp/src/app/app-routing/app-routing.module.ts
+++ Slay/ClientApp/src/app/app-routing/app-routing.module.ts
@@ -1,28 +1,25 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent }      from '../components/profile/profile.component';
-import { ProfilePostsComponent } from '../components/profile/profile-posts/profile-posts.component';
-import { ScrapbookComponent } from '../components/profile/scrapbook/scrapbook.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ProfileComponent } from "../components/profile/profile.component";
+import { ProfilePostsComponent } from "../components/profile/profile-posts/profile-posts.component";
+import { ScrapbookComponent } from "../components/profile/scrapbook/scrapbook.component";
 
 const rootRoutes: Routes = [
-  { path: 'profile', component: ProfileComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
-];
-
-const profileRoutes: Routes = [
-  { 
-    path: 'profile',
+  {
+    path: ":userId",
     component: ProfileComponent,
     children: [
-      { path: 'posts', component: ProfilePostsComponent, outlet: 'profile' },
-      { path: 'scraps', component: ScrapbookComponent, outlet: 'profile' },
-      { path: '', pathMatch: 'full', redirectTo: 'posts' }
+      { path: "posts", component: ProfilePostsComponent },
+      { path: "scraps", component: ScrapbookComponent },
+      { path: "", component: ScrapbookComponent, pathMatch: "full" },
+      { path: "**", redirectTo: "" }
     ]
-   }
+  },
+  { path: "", redirectTo: "home", pathMatch: "full" }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(rootRoutes), RouterModule.forChild(profileRoutes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(rootRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
